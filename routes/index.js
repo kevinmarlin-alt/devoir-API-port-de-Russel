b0f89eb76d197d131d7ca7ac6e4e84197f9b035d
Kevin Marlin <kevin.marlin@gmail.com>
2026-02-12 10:56:32 +0100

    feat(db): install mongoose and connection with mongoDB are working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const userRoute = require('./users')
+ 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'API Port de Russel' });
 });
+
+router.use('/users', userRoute)
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+//const services = require('../services/users')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//route.put('/add', services.add)
 
 module.exports = router;
